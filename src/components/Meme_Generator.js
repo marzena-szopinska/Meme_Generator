a6ecd1ffb83b5db76dc83236bb8370f3a58c4875
Marzena <marzena.szopinska090@gmail.com>
2019-06-17 16:04:32 +0200

    added Header component to display things and Meme_Generator component to fetch data

---
+++ src/components/Meme_Generator.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Meme_Generator extends React.Component {
+
+    componentDidMount(){
+        // API call
+    }
+    
+    render() {
+        return (
+            <div>
+                Meme Generator  
+            </div>
+        );
+    }
+}
+
+export default Meme_Generator;
