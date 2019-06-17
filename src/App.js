4af016e0c479dc08352927f118d3b8497a0e225d
Marzena <marzena.szopinska090@gmail.com>
2019-06-17 15:51:22 +0200

    added necessary code to render my application

--- src/App.js
+++ src/App.js
@@ -1,1 +1,13 @@
+import React from 'react';
 
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        Some content
+      </div>
+    );
+  }
+}
+
+export default App;
